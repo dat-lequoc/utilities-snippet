c719daa40c04ee05860f1c65cb4f8fe31a167876
Dat LQ. <71183203+dat-lequoc@users.noreply.github.com>
2025-09-11 17:55:56 +0200

    Update runpod.sh

--- runpod.sh
+++ runpod.sh
@@ -1,6 +1,8 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 export PATH="/root/.local/bin:$PATH" && uv --version
 
-uv venv
+uv venv --python 3.12 --seed
 . .venv/bin/activate
-uv pip install transformers accelerate torch -U
+# uv pip install vllm --torch-backend=auto
+uv pip install transformers accelerate torch tqdm -U
+
